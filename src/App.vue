297683e3f76c5f1cb98cdf83ecc4e9718084402b
Carlos Tadeu <carlostadeu.dev@gmail.com>
2025-08-27 12:04:11 -0300

    adiciona Bootstrap e implementa funcionalidades de gerenciamento de tarefas

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,82 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import {reactive} from "vue";
+const estado = reactive({
+	filtro: "todas",
+	tarefa: [
+		{
+			titulo: "Estudar CSS",
+			concluida: false,
+		},
+		{
+			titulo: "Estudar Vue.js",
+			concluida: true,
+		},
+		{
+			titulo: "Estudar JavaScript",
+			concluida: false,
+		},
+	],
+});
+const getTarefasPendentes = () => {
+	return estado.tarefa.filter((tarefa) => !tarefa.concluida);
+};
+const getTarefasConcluidas = () => {
+	return estado.tarefa.filter((tarefa) => tarefa.concluida);
+};
+const getTarefasFiltradas = () => {
+	const {filtro} = estado;
+	switch (filtro) {
+		case "pendentes":
+			return getTarefasPendentes();
+		case "concluidas":
+			return getTarefasConcluidas();
+		default:
+			return estado.tarefa;
+	}
+};
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+	<div class="container">
+		<header class="p-5 mb-4 mt-4 bg-light rounded-3 shadow-sm">
+			<h1>Minhas tarefas</h1>
+			<p>Você possui {{ getTarefasPendentes().length }} tarefas pendentes</p>
+		</header>
+		<form action="">
+			<div class="row">
+				<div class="col">
+					<input type="text" class="form-control" placeholder="Digite uma tarefa" />
+				</div>
+				<div class="col-md-2">
+					<button type="submit" class="btn btn-primary">Adicionar</button>
+				</div>
+				<div class="col-md-2">
+					<select @change="(e) => (estado.filtro = e.target.value)" class="form-select">
+						<option value="todas">Todas tarefas</option>
+						<option value="pendentes">Pendentes</option>
+						<option value="concluidas">Concluídas</option>
+					</select>
+				</div>
+			</div>
+		</form>
+		<ul class="list-group mt-4">
+			<li class="list-group-item" v-for="tarefa in getTarefasFiltradas()">
+				<input
+					@change="(e) => (tarefa.concluida = e.target.checked)"
+					:checked="tarefa.concluida"
+					:id="tarefa.titulo"
+					type="checkbox"
+				/>
+				<label :class="{done: tarefa.concluida}" class="ms-3" :for="tarefa.titulo">{{
+					tarefa.titulo
+				}}</label>
+			</li>
+		</ul>
+	</div>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.done {
+	text-decoration: line-through;
 }
 </style>
